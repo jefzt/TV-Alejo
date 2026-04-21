2469236c3f280ffddb06a403df960e5774c3b534
jefzt <jefrylv10@gmail.com>
2026-04-21 00:25:20 -0500

    fix: remove duplicate TvMaze folder

--- TvMaze/scripts/persistance.js
+++
@@ -1,8 +0,0 @@
-export function save(key, value) {
-    localStorage.setItem(key, JSON.stringify(value));
-}
-
-export function load(key, fallback = null) {
-    const data = localStorage.getItem(key);
-    return data ? JSON.parse(data) : fallback;
-}
